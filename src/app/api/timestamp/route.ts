7678909207a3f98d893fa80cd02175210b51d990
kimizuka <f.kimizuka@gmail.com>
2023-07-28 15:35:58 +0900

    add header

--- src/app/api/timestamp/route.ts
+++ src/app/api/timestamp/route.ts
@@ -3,5 +3,12 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   return NextResponse.json({
     timestamp: Date.now()
+  }, {
+    status: 200,
+    headers: {
+      'Cache-Control': 'no-store',
+      'CDN-Cache-Control': 'no-store',
+      'Vercel-CDN-Cache-Control': 'no-store'
+    }
   });
 }
